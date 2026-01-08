60bd950cb6d8c63df609c5f2d85491fe5483e6c6
Rikov Dmitry <rikovdmitri39@gmail.com>
2026-01-08 10:58:45 +0300

    remove createBrowserRouter

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,14 +3,32 @@ import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 import { Provider } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import store from '../../services/store';
 import { useDispatch } from '../../services/store';
 import { checkUserAuth } from '../../services/slices/userSlice';
+import {
+  Modal,
+  IngredientDetails,
+  ModalOrderInfo,
+  ProtectedRoute
+} from '@components';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 
 const AppContent = () => {
   const dispatch = useDispatch();
+  const goBack = () => window.history.back();
 
   useEffect(() => {
     dispatch(checkUserAuth());
@@ -19,14 +37,90 @@ const AppContent = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Outlet />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />}>
+          <Route
+            path='ingredients/:id'
+            element={
+              <Modal title='' onClose={goBack}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+        </Route>
+        <Route path='feed'>
+          <Route index element={<Feed />} />
+          <Route path=':number' element={<ModalOrderInfo />} />
+        </Route>
+        <Route
+          path='login'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='register'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='forgot-password'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='reset-password'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='profile'>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders/:number'
+            element={
+              <ProtectedRoute>
+                <ModalOrderInfo />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
     </div>
   );
 };
 
 const App = () => (
   <Provider store={store}>
-    <AppContent />
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   </Provider>
 );
 
